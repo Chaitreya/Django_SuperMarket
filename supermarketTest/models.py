f501e64d16474a6417eacf3a217cfb619c528af2
Chaitreya <cbhelkar55@gmail.com>
2025-12-01 17:36:29 +0530

    Added gender ,DOB, created_at and updated_at in the Customer model

--- supermarketTest/models.py
+++ supermarketTest/models.py
@@ -6,11 +6,30 @@ class Category(models.Model):
     category_name = models.CharField(max_length=100)
     description = models.CharField(max_length=200)
 
+GENDER_CHOICES = {
+    ('Male', 'Male'),
+    ('Female', 'Female'),
+    ('Other', 'Other / Non-Binary')
+}
 class Customer(models.Model):
     name = models.CharField(max_length=20)
     email = models.EmailField()
     phone_number = models.IntegerField()
     address = models.CharField(max_length=100)
+    date_of_birth = models.DateTimeField(
+        auto_now_add=True,
+    )
+    gender = models.CharField(
+        max_length=10,
+        choices=GENDER_CHOICES,
+        default='Other'
+    )
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+    )
+    updated_at = models.DateTimeField(
+        auto_now_add=True,
+    )
 
 
 class Product(models.Model):
